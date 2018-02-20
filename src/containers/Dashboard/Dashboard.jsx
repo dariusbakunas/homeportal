51c4a4a721e794d4e03ce09be273e19e734a0ca2
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-02-19 22:02:22 -0500

    restore 2 space indentation

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,71 +1,129 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react'
-import { Button, Container, Menu, Icon, Segment, Sidebar } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+import * as mainActions from '../Main/actions';
+import {
+	Button,
+	Container,
+	Menu,
+	Icon,
+	Segment,
+	Sidebar,
+	Drop,
+	Modal
+} from 'semantic-ui-react'
 
 class Dashboard extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      sideBarVisible: false,
-    }
-  }
+		this.state = {
+			sideBarVisible: false,
+			signOutModalOpen: false,
+		}
+	}
 
-  toggleSidebar = () => {
-    this.setState((prevState) => {
-      return {
-        sideBarVisible: !prevState.sideBarVisible,
-      }
-    });
-  };
+	toggleSidebar = () => {
+		this.setState((prevState) => {
+			return {
+				sideBarVisible: !prevState.sideBarVisible,
+			}
+		});
+	};
 
-  render() {
-    return (
-      <React.Fragment>
-        <Menu inverted fixed="top">
-          <Menu.Item>
-            <Button secondary icon onClick={this.toggleSidebar}>
-              <Icon name='sidebar' />
-            </Button>
-          </Menu.Item>
-          <Menu.Item header>
-            Home Portal
-          </Menu.Item>
-        </Menu>
-        <Sidebar.Pushable as={Segment} attached="bottom">
-          <Sidebar
-            as={Menu}
-            animation='overlay'
-            width='thin'
-            visible={this.state.sideBarVisible}
-            icon='labeled'
-            style={{ paddingTop: '3em' }}
-            vertical>
-            <Menu.Item name='home'>
-              <Icon name='home' />
-              Home
-            </Menu.Item>
-            <Menu.Item name='home'>
-              <Icon name='desktop' />
-              VMs
-            </Menu.Item>
-            <Menu.Item name='home'>
-              <Icon name='disk outline' />
-              Storage
-            </Menu.Item>
-          </Sidebar>
-          <Sidebar.Pusher
-            dimmed={this.state.sideBarVisible}
-            style={{ paddingTop: '4em' }}
-            onClick={this.toggleSidebar}>
-            <Container>
-              <div>DASHBOARD</div>
-            </Container>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </React.Fragment>
-    )
-  }
+	showSignOutModal = () => {
+		this.setState({
+			signOutModalOpen: true,
+		});
+	};
+
+	closeSignOutModal = () => {
+		this.setState({
+			signOutModalOpen: false,
+		});
+	};
+
+	closeSidebar = () => {
+		this.setState({
+			sideBarVisible: false,
+		});
+	};
+
+	render() {
+		return (
+			<React.Fragment>
+				<Menu inverted fixed="top">
+					<Menu.Item icon onClick={this.toggleSidebar}>
+						<Icon name='sidebar'/>
+					</Menu.Item>
+					<Menu.Item header>
+						Home Portal
+					</Menu.Item>
+					<Menu.Menu position="right">
+						<Menu.Item icon onClick={this.showSignOutModal}>
+							<Icon name='power'/>
+						</Menu.Item>
+					</Menu.Menu>
+				</Menu>
+				<Sidebar.Pushable as={Segment} attached="bottom">
+					<Sidebar
+						as={Menu}
+						animation='overlay'
+						width='thin'
+						visible={this.state.sideBarVisible}
+						icon='labeled'
+						style={{ paddingTop: '3em' }}
+						vertical>
+						<Menu.Item as='a' name='home'>
+							<Icon name='home' />
+							Home
+						</Menu.Item>
+						<Menu.Item name='home'>
+							<Icon name='desktop' />
+							VMs
+						</Menu.Item>
+						<Menu.Item name='home'>
+							<Icon name='disk outline' />
+							Storage
+						</Menu.Item>
+					</Sidebar>
+					<Sidebar.Pusher
+						dimmed={this.state.sideBarVisible}
+						style={{ paddingTop: '4em' }}
+						onClick={this.closeSidebar}>
+						<Container>
+							<div>DASHBOARD</div>
+						</Container>
+						<Modal open={this.state.signOutModalOpen} size='mini' onClose={this.closeSignOutModal}>
+							<Modal.Header>
+								Confirmation
+							</Modal.Header>
+							<Modal.Content>
+								<p>Are you sure you want to sign out?</p>
+							</Modal.Content>
+							<Button.Group attached='bottom' basic>
+								<Button onClick={this.closeSignOutModal}>
+									Cancel
+								</Button>
+								<Button onClick={this.props.actions.logout}>
+									Sign Out
+								</Button>
+							</Button.Group>
+						</Modal>
+					</Sidebar.Pusher>
+				</Sidebar.Pushable>
+			</React.Fragment>
+		)
+	}
 }
 
-export default Dashboard;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		actions: {
+			logout: () => {
+				dispatch(mainActions.logout.request());
+			}
+		}
+	};
+};
+
+export default connect(null, mapDispatchToProps)(Dashboard);
