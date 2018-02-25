b70be0fd60fad35a4576d80f701ca7e7aed5bb5f
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-02-25 17:34:14 -0500

    add sidebar link component

---
+++ client/src/containers/Dashboard/SidebarLink.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Menu,
+  Icon,
+} from 'semantic-ui-react';
+
+SidebarLink.propTypes = {
+  name: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  onNavigate: PropTypes.func,
+};
+
+function SidebarLink(props) {
+  const handleClick = () => {
+    if (props.onNavigate) {
+      props.onNavigate(props.url);
+    }
+  };
+
+  return (
+    <Menu.Item as='a' name={props.name} onClick={handleClick}>
+      <Icon name={props.icon} />
+      {props.label}
+    </Menu.Item>
+  )
+}
+
+export default SidebarLink;
