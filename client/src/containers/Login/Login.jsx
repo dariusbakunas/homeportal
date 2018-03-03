1549225749a38a969482e090127fcb21020ae004
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-03-02 21:00:44 -0500

    show simple login screen instead of going to auth0 right away

--- client/src/containers/Login/Login.jsx
+++ client/src/containers/Login/Login.jsx
@@ -3,13 +3,36 @@ import { Button } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import Logo from './Logo.png';
 import * as actions from "../Main/actions";
+import { getAuthInfo } from "../../utils/localStorage";
 
 class Login extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    const authInfo = getAuthInfo();
+
+    if (authInfo) {
+      const {fullName, accessToken, idToken, expiresAt} = authInfo;
+
+      this.props.actions.loginSuccess(
+        fullName,
+        accessToken,
+        idToken,
+        expiresAt,
+        this.getQueryParam(props, 'return'),
+      );
+    }
+  }
+
+  getQueryParam = (props, param) => {
+    const params = new URLSearchParams(props.location.search);
+    return params.get(param);
+  };
+
   handleLogin = () => {
-    const paramsString = this.props.location.search;
-    const params = new URLSearchParams(paramsString);
-    const returnUrl = params.get('return');
-    this.props.actions.login(returnUrl);
+    this.props.actions.login(
+      this.getQueryParam(this.props, 'return')
+    );
   };
 
   render() {
@@ -46,6 +69,9 @@ const mapDispatchToProps = (dispatch) => {
     actions: {
       login: (returnUrl) => {
         dispatch(actions.login.request(returnUrl));
+      },
+      loginSuccess: (fullName, accessToken, idToken, expiresAt, returnUrl) => {
+        dispatch(actions.login.success(fullName, accessToken, idToken, expiresAt, returnUrl));
       },
     }
   }
