8f161c86934ba957cf0c4c2f024058a0654bd529
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-09-09 14:10:46 -0400

    AuthProvider using Context API

---
+++ client/src/containers/Auth/AuthProvider.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import Auth from "./Auth";
+
+const AuthContext = React.createContext();
+const auth = new Auth();
+
+class AuthProvider extends React.Component {
+  state = {
+    isAuthenticated: false,
+  };
+
+  renewToken= () => {
+    try {
+      const { idTokenPayload, accessToken, idToken, expiresIn } = auth.renewToken();
+      const expiresAt = JSON.stringify((expiresIn * 1000) + new Date().getTime());
+
+
+    } catch (err) {
+
+    }
+  };
+
+  handleAuth = (locationHash) => {
+    return new Promise((resolve, reject) => {
+      if (/access_token|id_token|error/.test(locationHash)) {
+        auth.handleAuthentication()
+          .then(({ idTokenPayload, accessToken, idToken, expiresIn }) => {
+            const expiresAt = JSON.stringify((expiresIn * 1000) + new Date().getTime());
+
+            localStorage.setItem('access_token', accessToken);
+            localStorage.setItem('id_token', idToken);
+            localStorage.setItem('expires_at', expiresAt);
+
+            this.setState(() => {
+              return {
+                isAuthenticated: true,
+                tokens: {
+                  accessToken,
+                  idToken,
+                }
+              }
+            }, () => {
+              resolve({ idTokenPayload, accessToken, idToken, expiresAt });
+            });
+          })
+          .catch((err) => {
+            this.setState(() => {
+              return {
+                isAuthenticated: false,
+                tokens: {},
+              }
+            }, () => {
+              reject(err);
+            });
+          });
+      } else {
+        reject(new Error('Auth: Invalid location hash'));
+      }
+    });
+  };
+
+  logout = () => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+
+    this.setState({
+      isAuthenticated: false,
+      tokens: {},
+    });
+  };
+
+  componentDidMount() {
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    if (expiresAt && new Date().getTime() < expiresAt) {
+      this.setState({
+        tokens: {
+          accessToken: localStorage.getItem('access_token'),
+          idToken: localStorage.getItem('id_token'),
+        },
+        isAuthenticated: true,
+      });
+    } else {
+      this.setState({
+        isAuthenticated: false,
+        tokens: {},
+      });
+    }
+  }
+
+  render() {
+    return (
+      <AuthContext.Provider value={{
+        login: auth.login,
+        handleAuth: this.handleAuth,
+        logout: this.logout,
+        isAuthenticated: this.state.isAuthenticated,
+        tokens: this.state.tokens,
+      }}>
+        {this.props.children}
+      </AuthContext.Provider>
+    )
+  }
+}
+
+export { AuthProvider, AuthContext }
